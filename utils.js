bce9aa083ab947fc7bfa9a5f4303656caddd10a2
NoemiCouch <Noemicouch@gmail.com>
2023-05-17 11:09:08 -0500

    adding code to the promt

--- utils.js
+++ utils.js
@@ -13,44 +13,179 @@ const delete_employee = "Delete An Employee";
 const delete_role = "Delete A Role";
 const quit = "Quit";
 
+const inquirer = require("inquirer");
 const con = require("./db/connection");
 
-function show(table) {
+function show(query, cb) {
   con
     .promise()
-    .query(`SELECT * from ${table}`)
+    .query(query)
     .then(([rows, fields]) => {
       console.table(rows);
+      cb();
     })
     .catch((err) => console.log(err));
   //.then(() => con.end());
 }
 
-function showAllDepartments() {
-  show("departments");
+function showAllDepartments(cb) {
+  show(`SELECT * from departments`, cb);
 }
-function showAllEmployees() {
-  show("employees");
+function showAllEmployees(cb) {
+  show(
+    `SELECT e.id, e.first_name, e.last_name, r.title AS job_title, r.salary,d.name AS department, m.first_name AS manager FROM employees AS e
+    LEFT JOIN roles AS r
+    ON e.role_id=r.id
+    LEFT JOIN departments AS d
+    ON r.department_id=d.id
+    LEFT JOIN employees AS m
+    ON e.manager_id=m.id
+    `,
+    cb
+  );
 }
-function showAllRoles() {
-  show("roles");
+function showAllRoles(cb) {
+  show(
+    `SELECT r.id, r.title, r.salary,d.name AS department
+    FROM roles AS r 
+    LEFT JOIN departments AS d 
+    ON r.department_id=d.id`,
+    cb
+  );
 }
 
-function showEmployeesByDepartment() {
-  console.log(view_employees_by_department);
+function showEmployeesByDepartment(callbackQuestionFunc) {
+  con
+    .promise()
+    .query("SELECT name FROM departments")
+    .then(([rows, fields]) => {
+      const departments = rows.map((r) => r.name);
+      inquirer
+        .prompt({
+          name: "department",
+          message: "Chose a department to see employees in:",
+          type: "list",
+          choices: departments,
+        })
+        .then(({ department }) => {
+          const query = `SELECT e.id, e.first_name, e.last_name, r.title AS job_title, r.salary,d.name AS department, m.first_name AS manager
+           FROM employees AS e
+            LEFT JOIN roles AS r
+            ON e.role_id=r.id
+            LEFT JOIN departments AS d
+            ON r.department_id=d.id
+            LEFT JOIN employees AS m
+            ON e.manager_id=m.id`;
+
+          con
+            .promise()
+            .query(query)
+            .then(([rows, fields]) => {
+              const emp = rows.filter((e) => e.department === department);
+              console.table(emp);
+
+              callbackQuestionFunc();
+            })
+            .catch((err) => console.log(err));
+        });
+    })
+    .catch((err) => console.log(err));
+  // chose department
+  // get employees from department
 }
-function showEmployeesByManager() {
-  console.log(view_employees_by_manager);
+function showEmployeesByManager(callbackQuestionFunc) {
+  con
+    .promise()
+    .query(
+      `SELECT e.id, e.first_name, e.last_name, m.first_name AS manager_firstName, m.last_name AS manager_lastName, m.id AS manager_id
+           FROM employees AS e
+            LEFT JOIN employees AS m
+            ON e.manager_id=m.id`
+    )
+    .then(([rows, fields]) => {
+      const managers = rows.filter((r) => r.manager_id);
+      const manDisplay = managers.map(
+        (m) => m.manager_firstName + " " + m.manager_lastName
+      );
+      console.table(manDisplay);
+      callbackQuestionFunc();
+    })
+    .catch((err) => console.log(err));
 }
+
 function addADepartment() {
   console.log("add a department");
 }
-function addAnEmployee() {
-  console.log("add an employee");
+function addAnEmployee(cb) {
+  inquirer
+    .prompt([
+      {
+        name: "firstName",
+        message: "What is the employees first name?",
+      },
+      {
+        name: "lastName",
+        message: "What is the employees last name?",
+      },
+    ])
+    .then((res) => {
+      let data = {
+        first_name: res.firstName,
+        last_name: res.lastName,
+      };
+      con
+        .promise()
+        .query(
+          "SELECT r.id, r.title, r.salary,d.name AS department FROM roles AS r LEFT JOIN departments AS d ON r.department_id=d.id"
+        )
+        .then(([row]) => {
+          const roleChoices = row.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+          inquirer
+            .prompt({
+              name: "role",
+              message: "What is this employee`s role?",
+              type: "list",
+              choices: roleChoices,
+            })
+            .then((res) => {
+              data.role_id = res.role;
+              con
+                .promise()
+                .query("SELECT * FROM employees")
+                .then(([row]) => {
+                  const managerChoices = row.map(
+                    ({ first_name, last_name, id }) => ({
+                      name: `${first_name} ${last_name}`,
+                      value: id,
+                    })
+                  );
+                  managerChoices.push({ name: "None", value: null });
+                  inquirer
+                    .prompt({
+                      name: "manager",
+                      message: "Who is this employee`s manager?",
+                      type: "list",
+                      choices: managerChoices,
+                    })
+                    .then((res) => {
+                      data.manager_id = res.manager;
+                      con
+                        .promise()
+                        .query("INSERT INTO employees SET ?", data)
+                        .then(() => {
+                          console.log("Employee added");
+                          cb();
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
-function addARole() {
-  console.log("add a role");
-}
+function addARole() {}
 function updateEmployeeRole() {
   console.log("update employee role");
 }
@@ -74,50 +209,51 @@ function stop() {
 function answerQuestion(question, callbackQuestionFunc) {
   switch (question) {
     case view_all_departments:
-      showAllDepartments();
+      showAllDepartments(callbackQuestionFunc);
       break;
     case view_all_employees:
-      showAllEmployees();
+      showAllEmployees(callbackQuestionFunc);
       break;
     case view_all_roles:
-      showAllRoles();
+      showAllRoles(callbackQuestionFunc);
       break;
     case view_employees_by_department:
-      showEmployeesByDepartment();
-      break;
+      return showEmployeesByDepartment(callbackQuestionFunc);
     case view_employees_by_manager:
-      showEmployeesByManager();
-      break;
+      return showEmployeesByManager(callbackQuestionFunc);
     case add_a_department:
-      addADepartment();
+      addADepartment(callbackQuestionFunc);
       break;
     case add_an_employee:
-      addAnEmployee();
+      addAnEmployee(callbackQuestionFunc);
       break;
     case add_a_role:
-      addARole();
+      addARole(callbackQuestionFunc);
       break;
     case update_an_employee_role:
-      updateEmployeeRole();
+      updateEmployeeRole(callbackQuestionFunc);
       break;
     case update_an_employee_manager:
-      updateEmployeeManager();
+      updateEmployeeManager(callbackQuestionFunc);
       break;
     case delete_department:
-      deleteDepartment();
+      deleteDepartment(callbackQuestionFunc);
       break;
     case delete_employee:
-      deleteEmployee();
+      deleteEmployee(callbackQuestionFunc);
       break;
     case delete_role:
-      deleteRole();
+      deleteRole(callbackQuestionFunc);
       break;
     case quit:
       return stop();
     default:
       return stop();
   }
-  return callbackQuestionFunc();
+  // delay a sec before calling the questions again so that we can finish printing the table
+  // setTimeout(() => {
+  //   return callbackQuestionFunc();
+  // }, 1000);
 }
 
 module.exports = {
